f93ddee6a297290f940e64046fbb36aa5a5d941c
Glitch (glitch-hello-website) <none>
2022-11-27 13:57:22 +0000

    🦀🐇 Checkpoint ./a9-assignment.md:270926/2401 ./v-playback.js:270926/235

---
+++ a9-assignment.md
@@ -0,0 +1,6 @@
+# Hand-Tracking ML
+
+sequence (or single frame) of hand tracking
+
+Marie Petit style: find new in the latent space
+Autoencode hand positions?
---
+++ v-playback.js
@@ -0,0 +1,10 @@
+// Widgets for recording and playing back hand data
+
+
+// Needs to 
+// - record many versions
+// - have re-usable labels
+// - record face or hand
+Vue.component("tracking-recorded", {
+  
+})
