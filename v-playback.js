621cb7d4fd09edd29030711da511aa632479ab86
Glitch (glitch-hello-website) <none>
2022-11-27 14:19:36 +0000

    🌹🏀 Checkpoint ./style.css:270926/167 ./v-playback.js:270926/1350

--- v-playback.js
+++ v-playback.js
@@ -9,7 +9,9 @@
 Vue.component("tracking-recorder", {
   template: `<div class="widget tracking-widget">
     <div class="controls">
-      <button :class="{active:isRecording}">⏺</button>
+      <button :class="{active:isRecording}" @click="toggleRecording">⏺</button>
+      <button :class="{active:recordFace}" @click="recordFace=!recordFace">😐</button>
+      <button :class="{active:recordHands}" @click="recordHands=!recordHands">🖐</button>
       
       <table>
         <tr v-for="recording in recordings">
@@ -19,9 +21,27 @@ Vue.component("tracking-recorder", {
     </div>
   </div>`,
   
-  
+  methods: {
+    toggleRecording() {
+      if (this.isRecording) {
+        // Stop recording
+         this.isRecording=false
+       
+      } else {
+        // Start recording
+        this.isRecording=true
+        this.currentRecording = {
+          hands: this.recordHands?[[]]:undefined,
+          face: this.recordFace?[[]]:undefined,
+        }
+      }
+    }
+  },
   data() {
     return {
+      recordHands: true,
+      recordFace: false,
+      currentRecording: undefined,
       isRecording: false,
       recordings: localStorage.getItem("recordings")
     }
